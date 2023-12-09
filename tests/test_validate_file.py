44c0d7def44b1f64760ea99f90b4dc8a05239c98
Will Thames <will@thames.id.au>
2023-12-09 19:27:51 +1000

    Update test suite ready for python 3.12
    Drop support for python 3.8 - it's almost end of life and doesn't
    support importlib.resources.files() - we could use the backport
    importlib_resources module but easier to drop support.
    
    This change will only get merged into 1.29 and beyond - the
    assumption being that anyone who's ready for kubernetes 1.29
    is ready for python 3.9+

--- tests/test_validate_file.py
+++ tests/test_validate_file.py
@@ -7,34 +7,34 @@ parent = os.path.dirname(__file__)
 def test_validate_resource_file():
     rc = kubernetes_validate.validate_file(os.path.join(parent, 'resource.yaml'), '1.22.0',
                                            strict=False, quiet=True, no_warn=True)
-    assert(rc == 0)
+    assert (rc == 0)
 
 
 def test_validate_multi_resource_file():
     rc = kubernetes_validate.validate_file(os.path.join(parent, 'kuard-all.yaml'), '1.22.0',
                                            strict=False, quiet=True, no_warn=True)
-    assert(rc == 1)
+    assert (rc == 1)
 
 
 def test_validate_strict_file():
     rc = kubernetes_validate.validate_file(os.path.join(parent, 'kuard-extra-property.yaml'), '1.22.0',
                                            strict=True, quiet=True, no_warn=True)
-    assert(rc == 1)
+    assert (rc == 1)
 
 
 def test_validate_invalid_file():
     rc = kubernetes_validate.validate_file(os.path.join(parent, 'kuard-invalid-type.yaml'), '1.22.0',
                                            strict=False, quiet=True, no_warn=True)
-    assert(rc == 1)
+    assert (rc == 1)
 
 
 def test_validate_madeup_file():
     rc = kubernetes_validate.validate_file(os.path.join(parent, 'kuard-made-up-kind.yaml'), '1.22.0',
                                            strict=False, quiet=True, no_warn=True)
-    assert(rc == 0)
+    assert (rc == 0)
 
 
 def test_validate_version_too_new():
     rc = kubernetes_validate.validate_file(os.path.join(parent, 'resource.yaml'), '1.99.0',
                                            strict=False, quiet=True, no_warn=True)
-    assert(rc == 2)
+    assert (rc == 2)
